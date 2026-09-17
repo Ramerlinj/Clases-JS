a835bdabd3d15c590c0396b5e74514e9aa0e94bc
Alex <layoavicente3027@gmail.com>
2026-09-17 14:54:44 -0400

    cambios de ramerlin

--- practicas/practica__del__dia1/ejercicios_operadores_logocos/javashcrip.js
+++ practicas/practica__del__dia1/ejercicios_operadores_logocos/javashcrip.js
@@ -2,8 +2,8 @@
 let numero1 = 32;
 let numero2 = 52;
 
+document.write(`${numero1} + ${numero2}`)
 let suma = numero1 + numero2;
 let resta = numero1 - numero2;
 let multiplicacion = numero1 * numero2;
 
-console.log("suma:", suma)
